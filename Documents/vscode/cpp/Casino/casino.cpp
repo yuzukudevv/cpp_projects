456445d597565417368d1c4e74c25945202bd340
yuzuku <wiktor.materna06@gmail.com>
2025-11-06 13:38:37 +0100

    Move Blackjack project into Casino folder

---
+++ Documents/vscode/cpp/Casino/casino.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+    
+
+int main() {
+    cout << "┌────────────────────────┐\n";
+    cout << "│ Welcome to the casino! │\n";
+    cout << "└────────────────────────┘\n";
+
+    cout << "┌────────────────────┐\n";
+    cout << "│ [1] - Blackjack │\n";
+    cout << "└────────────────────┘\n";
+
+    cout << "┌──────────────────────┐\n";
+    cout << "│ [2] - Leave & Payout │\n";
+    cout << "└──────────────────────┘\n";
+
+    int userInput;
+
+    
+    
+
+}
